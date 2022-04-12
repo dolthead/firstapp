b2f6839a28d4c76e48c476a95468c7f47c8266cc
Todd H <github@simplybest.com>
2022-04-12 10:16:22 -0600

    add page 2 and route to it from tab 1

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page2Page } from '../page2/page2.page';
 import { Tab1Page } from './tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+  },
+  {
+    path: 'page2',
+    component: Page2Page
   }
 ];
 
